e4a6909b15f82ba9a8a796dc9ff97625d9bd60df
nicknaky <nicknaky@users.noreply.github.com>
2015-08-22 12:22:49 -0700

    latestv2

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var app = angular.module("myApp", []);
 
 app.directive("searchInput", function() {
   return {
+    scope: {
+      var1: "=template-url",
+      var2: "=input-data"
+    }
     templateUrl: 'AutoCompleteInput.html'
   };
 });
