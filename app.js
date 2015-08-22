7ceb84582b28e13a223c9e9e5dc358544e7fb303
nicknaky <nicknaky@users.noreply.github.com>
2015-08-22 14:58:09 -0700

    latest auto search!

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var app = angular.module("myApp", []);
 
 app.controller("MainCtrl", ["$scope", function($scope) {
 
-  $scope.API = "aklwjgt35j091";
+  $scope.API = "AIzaSyAfxptdFcXScMEnAJhk-VNpRxr_ksPos8o";
 
   $scope.test = "Angular Works!";
   $scope.suggestions = ["hi", "say", "bye"];
+  $scope.dataList = ["air", "airplace", "alligator", "alaze", "away", "airbnb", "aaa", "aahh", "azole"];
 
 
 }]);
@@ -16,11 +17,33 @@ app.directive("searchInput", function() {
   return {
     scope: {
       placesMode: "=googlePlaces",
-      apiKey: "@"
+      apiKey: "@",
+      inputData: "="
     },
     templateUrl: 'AutoCompleteInput.html',
     link: function(scope) {
+      scope.searchText = "";
 
+      scope.query = function(input) {
+        console.log("scope.query is running");
+        console.log("scope.inputData: " + scope.inputData);
+        console.log("input: " + input);
+        var list = document.getElementById("resultsList");
+        while (list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+          for(var i=0; i<scope.inputData.length; i++) {
+            if(scope.inputData[i].includes(input)) {
+
+              var node = document.createElement("LI");
+              var textNode = document.createTextNode(scope.inputData[i]);
+              node.appendChild(textNode);
+              list.appendChild(node);
+            }
+          }
+
+
+      };
     }
   };
 });
