ecec419c0ba9096a47e608ac7c5eca9c94842c93
nicknaky <nicknaky@users.noreply.github.com>
2015-08-22 12:51:42 -0700

    latest

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var app = angular.module("myApp", []);
 
-app.directive("searchInput", function() {
-  return {
-    scope: {
-      var1: "=template-url",
-      var2: "=input-data"
-    }
-    templateUrl: 'AutoCompleteInput.html'
-  };
-});
+
 
 app.controller("MainCtrl", ["$scope", function($scope) {
   $scope.test = "Angular Works!";
+  $scope.suggestions = ["hi", "say", "bye"];
+
+
 }]);
+
+app.directive("searchInput", function() {
+  return {
+    scope: {
+      var1: "=suggestions"
+    },
+    templateUrl: 'AutoCompleteInput.html',
+    link: function(scope) {
+      scope.
+    }
+  };
+});
